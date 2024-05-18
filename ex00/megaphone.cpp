553c326bf4f326e282bb1f5c0483c907223a3805
Ryan Boudwin <rboudwin@student.hive.fi>
2024-05-18 15:25:12 +0300

    megaphone ready

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,27 @@
+#include <cstring>
+#include <iostream>
+
+int	main(int argc, char **argv)
+{
+	int i;
+	int k;
+
+	k = 0;
+	i = 1;
+	if (argc < 2)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	}
+	while (argv[i])
+	{
+		while (argv[i][k])
+		{
+			std::cout << (char)std::toupper(argv[i][k]);
+			k++;
+		}
+		
+		k = 0;
+		i++;
+	}
+	std::cout << std::endl;
+}
